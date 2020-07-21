6a84b7860663ef1c68266e426a628fef2c1a257e
sooooz <soojinpar1026@gmail.com>
2020-07-21 21:50:22 +0900

    Android(15)_Network_Retrofit

--- AndroidStudyProject/app/src/main/java/com/example/myapplication/NetworkActivity.kt
+++ AndroidStudyProject/app/src/main/java/com/example/myapplication/NetworkActivity.kt
@@ -3,6 +3,14 @@ package com.example.myapplication
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_network.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -14,15 +22,22 @@ class NetworkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network)
 
-        NetworkTask().execute()
+        NetworkTask(
+            network_recyclerview,
+            LayoutInflater.from(this)
+        ).execute()
     }
 }
 
 // MainThread 정지 시킬 수 X -> 네트워크는 반드시 AsyncTask 필요
 // 네트워크를 MainThread에서 사용 -> ERROR
 
-class NetworkTask(): AsyncTask<Any?, Any?, Any?>() {
-    override fun doInBackground(vararg params: Any?): Any? {
+class NetworkTask(
+    val recyclerview: RecyclerView,
+    val inflater: LayoutInflater
+): AsyncTask<Any?, Any?, Array<PersonFromServer>>() {
+
+    override fun doInBackground(vararg params: Any?): Array<PersonFromServer> {
         val urlString: String = "http://mellowcode.org/json/students/"
         // url 객체로 변환
         val url: URL = URL(urlString)
@@ -44,11 +59,52 @@ class NetworkTask(): AsyncTask<Any?, Any?, Any?>() {
             buffer = reader.readLine()
         }
 
-        val temp = buffer.split(',')
+        val data = Gson().fromJson(buffer, Array<PersonFromServer>::class.java)
 
-        return null
+        return data
+    }
+
+    // UI thread에 접근 가능하기 때문에 여기에서 뷰를 그림
+    override fun onPostExecute(result: Array<PersonFromServer>?) {
+       val adapter = PersonAdapter(result!!, inflater)
+        recyclerview.adapter = adapter
+        super.onPostExecute(result)
     }
 }
 
 // 실제로 날아오는 데이터 -> byte 단위
 // UTF-8 표준 방식, BufferedReader 사용
+
+class PersonAdapter (
+    val personList: Array<PersonFromServer>,
+    val inflater: LayoutInflater
+): RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemview: View): RecyclerView.ViewHolder(itemview) {
+        val name: TextView
+        val age: TextView
+        val intro: TextView
+
+        init {
+            name = itemview.findViewById(R.id.person_name)
+            age = itemview.findViewById(R.id.person_age)
+            intro = itemview.findViewById(R.id.person_ment)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return personList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.item_view_network, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.setText(personList.get(position).name ?: "")
+        holder.age.setText(personList.get(position).age.toString())
+        holder.intro.setText(personList.get(position).intro ?: "")
+    }
+}
+
---
+++ AndroidStudyProject/app/src/main/java/com/example/myapplication/RetrofitActivity.kt
@@ -0,0 +1,87 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit)
+
+        // baseUrl() -> 서버 도메인 주소 부분 (변하지 않는 부분)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+        val service = retrofit.create(RetrofitService::class.java)
+
+        // GET 요청
+        service.getStudentsList().enqueue(object : Callback<ArrayList<PersonFromServer>> {
+            override fun onFailure(call: Call<ArrayList<PersonFromServer>>, t: Throwable) {
+                // 보통 error massage 넣음
+            }
+
+            override fun onResponse(
+                call: Call<ArrayList<PersonFromServer>>,
+                response: Response<ArrayList<PersonFromServer>>
+            ) {
+                if (response.isSuccessful) {
+                    val personList = response.body()
+                    val statusCode = response.code()
+                    val error = response.errorBody()
+                    val header = response.headers()
+                }
+            }
+        })
+
+        // POST 요청 (1)
+//        val params = HashMap<String, Any>()
+//        params.put("name", "김")
+//        params.put("age", 20)
+//        params.put("intro", "안녕하세요")
+//
+//        service.createStudent(params).enqueue(object : Callback<PersonFromServer> {
+//            override fun onFailure(call: Call<PersonFromServer>, t: Throwable) {
+//            }
+//
+//            override fun onResponse(
+//                call: Call<PersonFromServer>,
+//                response: Response<PersonFromServer>
+//            ) {
+//                if (response.isSuccessful) {
+//                    val person = response.body()
+//                    Log.d("retrofitt", "name: "+person?.name)
+//                }
+//            }
+//        })
+
+        // POST 요청 (2)
+        val person = PersonFromServer(name = "철수", age = 23, intro = "안녕하십니까")
+        service.createStudentEasy(person).enqueue(object : Callback<PersonFromServer> {
+            override fun onFailure(call: Call<PersonFromServer>, t: Throwable) {
+            }
+
+            override fun onResponse(
+                call: Call<PersonFromServer>,
+                response: Response<PersonFromServer>
+            ) {
+                if (response.isSuccessful) {
+                    val person = response.body()
+                    Log.d("retrofitt", "name: "+person?.name)
+                    Log.d("retrofitt", "age: "+person?.age)
+                    Log.d("retrofitt", "intro: "+person?.intro)
+                }
+            }
+        })
+
+    }
+}
