fafcf73fc7f2857e2c5fdec371910b1dd15ee067
sooooz <soojinpar1026@gmail.com>
2020-09-20 03:40:44 +0900

    add custom dialog

--- Android_Quiz_toyproject/app/src/main/java/com/example/toyproject/SolveQuizActivity.kt
+++ Android_Quiz_toyproject/app/src/main/java/com/example/toyproject/SolveQuizActivity.kt
@@ -1,11 +1,15 @@
 package com.example.toyproject
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_solve_quiz.*
 import retrofit2.Call
@@ -17,15 +21,20 @@ class SolveQuizActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solve_quiz)
 
-        var myAnswerList = arrayListOf(-1, -1, -1, -1, -1)
-        val nickname: String = "test9"
+        lateinit var quizList: ArrayList<Quiz>      // 풀 퀴즈 리스트
+        var quizAnswerList = arrayListOf<Int>()     // 퀴즈 주인 답 리스트
+        var myAnswerList = arrayListOf(-1, -1, -1, -1, -1)  // 내가 푼 답 리스트
+        val nickname: String = "test9"          // 현재 풀고있는 퀴즈 주인 닉네임
 
+        // 현재 풀고있는 퀴즈 주인 닉네임 설정
         solve_nick.setText(nickname).toString()
 
+        // 해당 닉네임의 퀴즈 불러옴
         (application as MasterApplication).service.getNicknameQuiz(nickname)
             .enqueue(object: Callback<QuizList> {
                 override fun onFailure(call: Call<QuizList>, t: Throwable) {
                     Toast.makeText(this@SolveQuizActivity, "Quiz 실패", Toast.LENGTH_LONG).show()
+                    finish()
                 }
 
                 override fun onResponse(
@@ -33,68 +42,60 @@ class SolveQuizActivity : AppCompatActivity() {
                     response: Response<QuizList>
                 ) {
                     if (response.isSuccessful) {
-                        val quizList = response.body()
-
+                        val quizListList = response.body()
+                        quizList = quizListList!!.quizList      // quizList 초기화
                         Toast.makeText(this@SolveQuizActivity, "Quiz 성공", Toast.LENGTH_LONG).show()
 
-                        val solveQuizAdapter = SolveQuizAdapter(quizList!!.quizList, LayoutInflater.from(this@SolveQuizActivity), myAnswerList)
+                        val solveQuizAdapter = SolveQuizAdapter(quizList, LayoutInflater.from(this@SolveQuizActivity), myAnswerList)
                         solve_recyclerview.adapter = solveQuizAdapter
                         solve_recyclerview.layoutManager = LinearLayoutManager(this@SolveQuizActivity)
-
-                        solve_btn.setOnClickListener {
-                            if (-1 in myAnswerList) {
-                                // 풀지 않은 퀴즈가 있을 경우 toast
-                                Toast.makeText(this@SolveQuizActivity, "풀지 않은 문제가 있습니다", Toast.LENGTH_LONG).show()
-                            } else {    // 모든 퀴즈를 풀었을 경우 dialog
-
-                            }
-                        }
 
                     }
                 }
             })
 
-        /*
-        // 퀴즈 제출 버튼 클릭
-        solve_finish.setOnClickListener {
-            for (i in 0 until 5) {
-                Log.d("msgg", "" + quizList[i].answer)
-            }
-
-            // 이미 제출한 사용자일 경우
-            // if ()
-
-
-            var score: Int = 0
-
-            val builder = AlertDialog.Builder(this)
-            val dialogView = layoutInflater.inflate(R.layout.dialog_view, null)
-
-            // dialog score 변경
-            val dialogScore = dialogView.findViewById<TextView>(R.id.dialog_score)
-            dialogScore.setText("$score 점")
-
-            // dialog 띄움
-            builder.setView(dialogView)
-                .setPositiveButton("확인") { dialog, i ->
-                    //val date = dialogDate.text.toString()
-                    //val content = dialogContent.text.toString()
-                    //planList.add(Plan(date, content))
-
-
-                    // score 전달
-                    val intent = Intent(this, MyifoActivity::class.java)
-                    intent.apply {
-                        this.putExtra("score", score)
-                    }
-                    startActivityForResult(intent, 200)
-
-                    // val num1 = intent.getIntExtra("num1", 0)
-
+        // 제출 버튼을 클릭했을 경우
+        solve_btn.setOnClickListener {
+            if (-1 in myAnswerList) {   // 풀지 않은 퀴즈가 있을 경우 toast
+                Toast.makeText(this@SolveQuizActivity, "풀지 않은 문제가 있습니다", Toast.LENGTH_LONG).show()
+            } else {    // 모든 퀴즈를 풀었을 경우 dialog
+                for (i in 0 until 5) {
+                    quizAnswerList.add(i, quizList[i].answer)   // 퀴즈 주인 답 리스트 초기화
                 }
-                .show()
-        }
-        */
 
+                val score = calcScore(quizAnswerList, myAnswerList)     // 퀴즈 score 계산
+                setDialog(score)    // dialog 설정
+            }
+        }
+
+    }
+
+    // score 계산하는 함수
+    private fun calcScore(quizAnswerList: ArrayList<Int>, myAnswerList: ArrayList<Int>): Int {
+        var score = 0
+
+        for (i in 0 until quizAnswerList.size) {
+            if (quizAnswerList[i] == myAnswerList[i])
+                score += 20
+        }
+        return score
+    }
+
+    // 퀴즈 모두 푼 후 사용되는 dialog
+    private fun setDialog(score: Int) {
+        val builder = AlertDialog.Builder(this@SolveQuizActivity)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_view, null)
+
+        // dialog score 변경
+        val dialogScore = dialogView.findViewById<TextView>(R.id.solve_score)
+        dialogScore.setText("$score 점").toString()
+
+        val okBtn = dialogView.findViewById<Button>(R.id.solve_dialog_btn)
+        okBtn.setOnClickListener {
+            // test activity로 넘어감
+            startActivity(Intent(this@SolveQuizActivity, TestActivity::class.java))
+        }
+
+        builder.setView(dialogView).show()
     }
 }
