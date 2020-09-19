3adbda505785562c18d8812db9aa937f402cdac9
sooooz <soojinpar1026@gmail.com>
2020-09-19 23:35:31 +0900

    use SharedPreferences

--- Android_BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ Android_BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.bmicalculator
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -11,7 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadData()  // 이전에 입력한 값 불러옴
+
         resultBtn.setOnClickListener {
+
+            // 마지막에 입력한 데이터 저장
+            saveData(heightEditText.text.toString().toInt(), weightEditText.text.toString().toInt())
+
             /*
             val intent = Intent(this, ResultActivity::class.java)
             intent.putExtra("height", heightEditText.text.toString())
@@ -27,6 +35,25 @@ class MainActivity : AppCompatActivity() {
                 "weight" to weightEditText.text.toString()
             )
         }
+    }
 
+    private fun saveData(height: Int, weight: Int) {
+        val sp = getSharedPreferences("sp", Context.MODE_PRIVATE)   // SharedPreferences 객체 생성
+        val editor = sp.edit()
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+    }
+
+    private fun loadData() {
+        val sp = getSharedPreferences("sp", Context.MODE_PRIVATE)   // SharedPreferences 객체 생성
+        val height = sp.getInt("KEY_HEIGHT", 0)
+        val weight = sp.getInt("KEY_WEIGHT", 0)
+
+        // 저장된 값이 있다면 저장된 값으로 설정
+        if (height != 0 && weight != 0) {
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
     }
 }
