c5f6b1045d778be0cf4c62a2ef5bd5ff2eb2aec4
sooooz <soojinpar1026@gmail.com>
2020-09-21 00:06:02 +0900

    add timer RESET and LAPTIME function

--- Android_StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ Android_StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timer
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private var isRunning = false   // 타이머가 동작 중인지 아닌지 저장하는 변수
     private var timerTask: Timer? = null    // timer 취소를 위해 변수 선언
                                             // timer를 실행하고 반환하는 Timer 객체를 저장
+    private var lap = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,14 @@ class MainActivity : AppCompatActivity() {
                 start()
             else
                 pause()
+        }
+
+        lapBtn.setOnClickListener {
+            recordLapTime()
+        }
+
+        resetFab.setOnClickListener {
+            reset()
         }
 
     }
@@ -54,4 +64,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun recordLapTime() {
+        val lapTime = time     // this.time
+        val textView = TextView(this)
+        textView.text = "$lap LAP : ${lapTime / 100}.${lapTime % 100}"
+
+        // LinearLayout의 맨 위에 textView 추가
+        // addView(): 두 번째 인자에 인덱스 값 지정하면 해당 위치에 뷰 추가됨
+        lapLayout.addView(textView, 0)
+        lap++
+    }
+
+    private fun reset() {
+        timerTask?.cancel()
+
+        // 모든 변수 초기화
+        time = 0
+        isRunning = false
+        playFab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        secTextView.text = "0"
+        milliTextView.text = "00"
+
+        // 모든 랩타임 제거
+        lapLayout.removeAllViews()
+        lap = 1
+    }
 }
