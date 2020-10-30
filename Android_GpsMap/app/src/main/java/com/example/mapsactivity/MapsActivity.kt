8e94e5751d4bcaf0b4e2be84daa789790ae4d92e
sooooz <soojinpar1026@gmail.com>
2020-10-30 15:37:20 +0900

    get google map with Google API

--- Android_GpsMap/app/src/main/java/com/example/mapsactivity/MapsActivity.kt
+++ Android_GpsMap/app/src/main/java/com/example/mapsactivity/MapsActivity.kt
@@ -2,6 +2,10 @@ package com.example.mapsactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,6 +18,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: MyLocationCallBack
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -21,6 +29,60 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationInit()
+    }
+
+    // 위치 정보를 얻기 위한 각종 초기화
+    private fun locationInit() {
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        locationCallback = MyLocationCallBack()
+
+        locationRequest = LocationRequest()
+
+        // GPS 우선
+        // priority = 정확도
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        // 업데이트 인터벌
+        // 위치 정보가 없을 때는 업데이트 안 함
+        // 상황에 따라 짧아질 수 있음, 정확하지 않음
+        // 다른 앱에서 짧은 인터벌로 위치 정보를 요청하면 짧아질 수 있음
+        locationRequest.interval = 10000
+
+        // 정확함, 이것보다 짧은 업데이트는 하지 않음
+        locationRequest.fastestInterval = 5000
+
+        // -> 이 요청은 GPS를 사용하여 가장 정확한 위치를 요구하면서
+        // 10초마다 위치 정보를 갱신함
+        // 그 사이에 다른 앱에서 위치를 갱신했다면 5초마다 확인하여
+        // 그 값을 활용하여 베터리를 절약함
+    }
+
+    inner class MyLocationCallBack: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            val location = locationResult?.lastLocation
+
+            location?.run {
+                // location 객체가 null이 아닐 때
+                // 14 level로 확대하고 현재 위치로 카메라 이동
+                val latLng = LatLng(latitude, longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        addLocationListener()
+    }
+
+    private fun addLocationListener() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
     }
 
     /**
