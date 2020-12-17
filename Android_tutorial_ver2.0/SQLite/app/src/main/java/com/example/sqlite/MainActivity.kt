34b721a3e32e929b6c94736a1928ef9b56bf6a6f
sooooz <soojinpar1026@gmail.com>
2020-12-17 15:57:40 +0900

    add SELECT, UPDATE and DELETE with sql

--- Android_tutorial_ver2.0/SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
+++ Android_tutorial_ver2.0/SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sqlite
 
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -32,7 +34,73 @@ class MainActivity : AppCompatActivity() {
 
             db.close()
 
-            textView.text = "complete"
+            textView.text = " insert complete"
+        }
+
+        btn2.setOnClickListener {
+            var helper: DBHelper = DBHelper(this)
+            var db: SQLiteDatabase = helper.writableDatabase
+
+            var sql = "select * from TestTable"
+
+            // Cursor class type의 객체
+            // select한 데이터에 접근해서 데이터 가져옴
+            // select -> db.rawQuery()
+            // insert, update, delete -> db.execSQL()
+            var c: Cursor = db.rawQuery(sql, null)
+
+            textView.text = ""
+
+            while (c.moveToNext()) {
+                // column name에 따른 해당 값 index 번호 반환
+                var idx_pos = c.getColumnIndex("idx")
+                var textData_pos = c.getColumnIndex("textData")
+                var intData_pos = c.getColumnIndex("intData")
+                var floatData_pos = c.getColumnIndex("floatData")
+                var dateData_pos = c.getColumnIndex("dateData")
+
+                var idx = c.getInt(idx_pos)
+                var textData = c.getString(textData_pos)
+                var intData = c.getInt(intData_pos)
+                var floatData = c.getDouble(floatData_pos)
+                var dateData = c.getString(dateData_pos)
+
+                textView.append("idx: $idx\n")
+                textView.append("textData: $textData\n")
+                textView.append("intData: $intData\n")
+                textView.append("floatData: $floatData\n")
+                textView.append("dateData: $dateData\n\n")
+            }
+
+            db.close()
+        }
+
+        btn3.setOnClickListener {
+            var helper = DBHelper(this)
+            var db = helper.writableDatabase
+
+            var sql = "update TestTable set textData=? where idx=?"
+            var args = arrayOf("arg3", "1")     // 쿼리문에 작성되어 있는 물음표 순서에 맞춰서 setting
+
+            db.execSQL(sql, args)
+
+            db.close()
+
+            textView.text = "update complete"
+        }
+
+        btn4.setOnClickListener {
+            var helper = DBHelper(this)
+            var db = helper.writableDatabase
+
+            var sql = "delete from TestTable where idx=?"
+            var args = arrayOf("1")
+
+            db.execSQL(sql, args)
+
+            db.close()
+
+            textView.text = "delete complete"
         }
 
     }
