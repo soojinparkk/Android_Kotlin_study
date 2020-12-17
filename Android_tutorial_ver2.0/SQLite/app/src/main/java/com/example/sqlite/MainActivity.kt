f1297895c052ff174895b1b78ea76b93dd2d2b5c
sooooz <soojinpar1026@gmail.com>
2020-12-17 23:00:07 +0900

    add a method using Class without using SQL

--- Android_tutorial_ver2.0/SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
+++ Android_tutorial_ver2.0/SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sqlite
 
+import android.content.ContentValues
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
@@ -19,18 +20,42 @@ class MainActivity : AppCompatActivity() {
             // DB에 접근해서 사용할 수 있는 객체 얻어옴 == open
             var db = helper.writableDatabase
 
-            var sql = "insert into TestTable (textData, intData, floatData, dateData) values (?, ?, ?, ?)"
+            // 1. SQL 쿼리문 직접 작성
 
-            // 해당 양식에 맞춰서 문자열로 반환
+//            var sql = "insert into TestTable (textData, intData, floatData, dateData) values (?, ?, ?, ?)"
+//
+//            // 해당 양식에 맞춰서 문자열로 반환
+//            var sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+//            var date = sdf.format(Date())
+//
+//            var arg1 = arrayOf("arg1", "100", "11.11", date)
+//            var arg2 = arrayOf("arg2", "200", "22.22", date)
+//
+//            // 쿼리 실행
+//            db.execSQL(sql, arg1)
+//            db.execSQL(sql, arg2)
+
+            // 2. class 이용하여 작성
+
             var sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
             var date = sdf.format(Date())
 
-            var arg1 = arrayOf("arg1", "100", "11.11", date)
-            var arg2 = arrayOf("arg2", "200", "22.22", date)
+            // ContentValues(): 어떤 column에 어떤 값이 대응하는 지 지정 (= 일대일 대응)
+            var cv1 = ContentValues()
+            cv1.put("textData", "arg1")
+            cv1.put("intData", 100)
+            cv1.put("floatData", 11.11)
+            cv1.put("dateData", date)
 
-            // 쿼리 실행
-            db.execSQL(sql, arg1)
-            db.execSQL(sql, arg2)
+            db.insert("TestTable", null, cv1)
+
+            var cv2 = ContentValues()
+            cv2.put("textData", "arg2")
+            cv2.put("intData", 200)
+            cv2.put("floatData", 22.22)
+            cv2.put("dateData", date)
+
+            db.insert("TestTable", null, cv2)
 
             db.close()
 
@@ -41,13 +66,28 @@ class MainActivity : AppCompatActivity() {
             var helper: DBHelper = DBHelper(this)
             var db: SQLiteDatabase = helper.writableDatabase
 
-            var sql = "select * from TestTable"
+            // 1. SQL 쿼리문 직접 작성
 
-            // Cursor class type의 객체
-            // select한 데이터에 접근해서 데이터 가져옴
-            // select -> db.rawQuery()
-            // insert, update, delete -> db.execSQL()
-            var c: Cursor = db.rawQuery(sql, null)
+//            var sql = "select * from TestTable"
+//
+//            // Cursor class type의 객체
+//            // select한 데이터에 접근해서 데이터 가져옴
+//            // select -> db.rawQuery()
+//            // insert, update, delete -> db.execSQL()
+//            var c: Cursor = db.rawQuery(sql, null)
+
+            // 2. class 이용하여 작성
+
+            // 첫 번째 인자: 테이블 이름
+            // 두 번째 인자: 테이블에서 가져올 column 이름의 문자열 배열 (null == 모든 값)
+            // 세 번째 인자: 조건절 ex) "a=? and b=?"
+            // -> ?에 10을 넣으면 a라는 column의 값이 10인 레코드 가져옴
+            // -> (null == 조건절이 없을 경우)
+            // 네 번째 인자: 위의 조건절의 ?에 설정될 값의 문자열 배열
+            // 다섯 번째 인자: 정렬 기준
+            // 여섯 번째 인자: group by
+            // 일곱 번째 인자: having
+            var c = db.query("TestTable", null, null, null, null, null, null)
 
             textView.text = ""
 
@@ -79,10 +119,21 @@ class MainActivity : AppCompatActivity() {
             var helper = DBHelper(this)
             var db = helper.writableDatabase
 
-            var sql = "update TestTable set textData=? where idx=?"
-            var args = arrayOf("arg3", "1")     // 쿼리문에 작성되어 있는 물음표 순서에 맞춰서 setting
+            // 1. SQL 쿼리문 직접 작성
 
-            db.execSQL(sql, args)
+//            var sql = "update TestTable set textData=? where idx=?"
+//            var args = arrayOf("arg3", "1")     // 쿼리문에 작성되어 있는 물음표 순서에 맞춰서 setting
+//
+//            db.execSQL(sql, args)
+
+            // 2. class 이용하여 작성
+
+            var cv = ContentValues()
+            cv.put("textData", "arg3")
+            var where = "idx=?"
+            var args = arrayOf("1")
+
+            db.update("TestTable", cv, where, args)
 
             db.close()
 
@@ -93,10 +144,19 @@ class MainActivity : AppCompatActivity() {
             var helper = DBHelper(this)
             var db = helper.writableDatabase
 
-            var sql = "delete from TestTable where idx=?"
+            // 1. SQL 쿼리문 직접 작성
+
+//            var sql = "delete from TestTable where idx=?"
+//            var args = arrayOf("1")
+//
+//            db.execSQL(sql, args)
+
+            // 2. class 이용하여 작성
+
+            var where = "idx=?"
             var args = arrayOf("1")
 
-            db.execSQL(sql, args)
+            db.delete("TestTable", where, args)
 
             db.close()
 
